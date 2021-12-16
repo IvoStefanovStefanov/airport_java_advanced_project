a85ff40cfe7f3b66d6b9eea153019ae22a9ca1fb
IvoStefanovStefanov <ivo_stefanov@gbg.bg>
2021-12-16 10:33:22 +0200

    Initial commit maessage

---
+++ src/main/java/com/aacademy/airport_java_advanced_project/AirportJavaAdvancedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aacademy.airport_java_advanced_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportJavaAdvancedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportJavaAdvancedProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aacademy/airport_java_advanced_project/AirportJavaAdvancedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aacademy.airport_java_advanced_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportJavaAdvancedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
